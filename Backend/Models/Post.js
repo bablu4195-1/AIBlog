d4e6dd514d49aab2fb9c373d0f3c45f66968238c
Venkata Sai Teja Mukkapati <55186465+bablu4195-1@users.noreply.github.com>
2023-07-01 19:00:48 +0530

    adding posts

--- Backend/Models/Post.js
+++ Backend/Models/Post.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const PostSchema = new Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
